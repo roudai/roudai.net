c66363d350dab13641fb2270efa7ff31e9e8ff9f
roudai <me@roudai.net>
2022-03-10 00:23:32 +0900

    NavBar変更

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,55 +1,67 @@
 <template>
   <div>
-    <b-navbar type="dark" variant="dark">
-      <b-navbar-nav>
-        <b-nav-item to="/">roudainet</b-nav-item>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand href="#">roudai.net</b-navbar-brand>
 
-        <!-- Navbar dropdowns -->
-        <b-nav-item-dropdown text="F2L" left>
-          <b-dropdown-item to="/f2l/f2l">F2L手順</b-dropdown-item>
-          <b-dropdown-item to="/f2l/f2l-advanced">発展形F2L</b-dropdown-item>
-          <b-dropdown-item to="/f2l/winter-variation">Winter Variation</b-dropdown-item>
-        </b-nav-item-dropdown>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-        <b-nav-item-dropdown text="OLL" left>
-          <b-dropdown-item to="/oll/oll-view">OLL一覧</b-dropdown-item>
-          <b-dropdown-item to="/oll/oll">OLL手順</b-dropdown-item>
-          <b-dropdown-item to="/oll/coll">COLL</b-dropdown-item>
-        </b-nav-item-dropdown>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item-dropdown text="F2L" left>
+            <b-dropdown-item to="/f2l/f2l">F2L手順</b-dropdown-item>
+            <b-dropdown-item to="/f2l/f2l-advanced">発展形F2L</b-dropdown-item>
+            <b-dropdown-item to="/f2l/winter-variation">Winter Variation</b-dropdown-item>
+          </b-nav-item-dropdown>
 
-        <!-- <b-nav-item-dropdown text="PLL" left>
-          <b-dropdown-item to="#">PLL一覧</b-dropdown-item>
-          <b-dropdown-item to="#">PLLの確率</b-dropdown-item>
-          <b-dropdown-item to="#">PLL手順</b-dropdown-item>
-        </b-nav-item-dropdown>
+          <b-nav-item-dropdown text="OLL" left>
+            <b-dropdown-item to="/oll/oll-view">OLL一覧</b-dropdown-item>
+            <b-dropdown-item to="/oll/oll">OLL手順</b-dropdown-item>
+            <b-dropdown-item to="/oll/coll">COLL</b-dropdown-item>
+          </b-nav-item-dropdown>
 
-        <b-nav-item-dropdown text="BLD" left>
-          <b-dropdown-item to="#">ルービックキューブを目隠しで揃えるには</b-dropdown-item>
-          <b-dropdown-item to="#">ルービックキューブ目隠しの解法</b-dropdown-item>
-          <b-dropdown-item to="#">Old Pochmann method (corner)</b-dropdown-item>
-        </b-nav-item-dropdown>
+          <b-nav-item-dropdown text="PLL" left>
+            <b-dropdown-item to="/pll/pll-view">PLL一覧</b-dropdown-item>
+            <b-dropdown-item to="/pll/pll-chances">PLLの確率</b-dropdown-item>
+            <b-dropdown-item to="/pll/pll">PLL手順</b-dropdown-item>
+          </b-nav-item-dropdown>
 
-        <b-nav-item-dropdown text="Megaminx" left>
-          <b-dropdown-item to="#">Megaminx EO</b-dropdown-item>
-          <b-dropdown-item to="#">Megaminx CO</b-dropdown-item>
-          <b-dropdown-item to="#">Megaminx EP</b-dropdown-item>
-          <b-dropdown-item to="#">Megaminx CP</b-dropdown-item>
-        </b-nav-item-dropdown>
+          <b-nav-item-dropdown text="BLD" left>
+            <b-dropdown-item to="#">ルービックキューブを目隠しで揃えるには</b-dropdown-item>
+            <b-dropdown-item to="#">ルービックキューブ目隠しの解法</b-dropdown-item>
+            <b-dropdown-item to="#">Old Pochmann method (corner)</b-dropdown-item>
+          </b-nav-item-dropdown>
 
-        <b-nav-item-dropdown text="FMC" left>
-          <b-dropdown-item to="#">ルービックキューブ最少手数競技の紹介</b-dropdown-item>
-        </b-nav-item-dropdown>
+          <b-nav-item-dropdown text="Megaminx" left>
+            <b-dropdown-item to="#">Megaminx EO</b-dropdown-item>
+            <b-dropdown-item to="#">Megaminx CO</b-dropdown-item>
+            <b-dropdown-item to="#">Megaminx EP</b-dropdown-item>
+            <b-dropdown-item to="#">Megaminx CP</b-dropdown-item>
+          </b-nav-item-dropdown>
 
-        <b-nav-item-dropdown text="Tools" left>
-          <b-dropdown-item href="https://visualcube.roudai.net">VisualCube Editor</b-dropdown-item>
-        </b-nav-item-dropdown>
+          <b-nav-item-dropdown text="FMC" left>
+            <b-dropdown-item to="#">ルービックキューブ最少手数競技の紹介</b-dropdown-item>
+          </b-nav-item-dropdown>
 
-        <b-nav-item-dropdown text="Other" left>
-          <b-dropdown-item to="#">ルービックキューブ各種解法</b-dropdown-item>
-          <b-dropdown-item to="#">ルービックキューブの早解きについて知らない方々に知っておいて欲しいこと</b-dropdown-item>
-          <b-dropdown-item to="#">社会人がスピードキューブを続ける意味について</b-dropdown-item>
-        </b-nav-item-dropdown> -->
-      </b-navbar-nav>
+          <b-nav-item-dropdown text="Tools" left>
+            <b-dropdown-item href="https://visualcube.roudai.net">VisualCube Editor</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown text="Other" left>
+            <b-dropdown-item to="#">ルービックキューブ各種解法</b-dropdown-item>
+            <b-dropdown-item to="#">ルービックキューブの早解きについて知らない方々に知っておいて欲しいこと</b-dropdown-item>
+            <b-dropdown-item to="#">社会人がスピードキューブを続ける意味について</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <a href="https://github.com/roudai/roudai.net">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-github" viewBox="0 0 16 16">
+            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+            </svg>
+          </a>
+        </b-navbar-nav>
+      </b-collapse>
     </b-navbar>
   </div>
 </template>
