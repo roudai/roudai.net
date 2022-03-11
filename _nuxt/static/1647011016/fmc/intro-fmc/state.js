77447ccfd6edf7277af561aecf383eef8ae4cb8f
roudai <roudai@users.noreply.github.com>
2022-03-11 15:03:41 +0000

    deploy: e38ad58ff4eabac9af5405bdb79abf96af4ed678

---
+++ _nuxt/static/1647011016/fmc/intro-fmc/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Froudai.net\u002F_nuxt\u002Fstatic\u002F1647011016",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002Froudai.net\u002F",assetsPath:"\u002Froudai.net\u002F_nuxt\u002F",cdnURL:null}}}}("\u002Ffmc\u002Fintro-fmc"));
