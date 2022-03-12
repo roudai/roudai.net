d7ee3c71db56bc30885c2d8339342d9694b6df33
roudai <roudai@users.noreply.github.com>
2022-03-12 00:30:11 +0000

    deploy: 5c708e47a2c6d3c2a704abd87600ca2ddff0a159

---
+++ _nuxt/static/1647045005/other/advent-calendar2014/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Froudai.net\u002F_nuxt\u002Fstatic\u002F1647045005",layout:"default",error:a,serverRendered:true,routePath:"\u002Fother\u002Fadvent-calendar2014",config:{_app:{basePath:"\u002Froudai.net\u002F",assetsPath:"\u002Froudai.net\u002F_nuxt\u002F",cdnURL:a}}}}(null));
