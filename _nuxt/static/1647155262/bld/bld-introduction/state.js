316efc5899b31811c12ca0b7fca610d43728e600
roudai <roudai@users.noreply.github.com>
2022-03-13 07:07:48 +0000

    deploy: 83b2a01c4b7fc162559a0aa27d6da63a80b7c8bf

---
+++ _nuxt/static/1647155262/bld/bld-introduction/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Froudai.net\u002F_nuxt\u002Fstatic\u002F1647155262",layout:"default",error:a,serverRendered:true,routePath:"\u002Fbld\u002Fbld-introduction",config:{_app:{basePath:"\u002Froudai.net\u002F",assetsPath:"\u002Froudai.net\u002F_nuxt\u002F",cdnURL:a}}}}(null));
