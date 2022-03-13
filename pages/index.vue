3d6facd75aad5771098a274c2bc2af4141893a74
roudai <me@roudai.net>
2022-03-13 16:18:07 +0900

    トップページ

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,38 @@
 <template>
   <div>
     <h2>roudai.netとは</h2>
+    <p>
+      ルービックキューブの手順、解法その他についてまとめています。
+      中級～上級者向けのため、これからスピードキューブを始める初心者の方にはおすすめしません（というか見ても分かりません）。
+    </p>
+
+    <h5>管理人について</h5>
+    <p>
+      ハンドルネーム：ろーだい<br>
+      ツイッター：<a href="https://twitter.com/roudainet">@roudainet</a><br>
+      Youtube：<a href="http://www.youtube.com/user/roudai">http://www.youtube.com/user/roudai</a><br>
+      公式記録：<a href="https://www.worldcubeassociation.org/persons/2009ISHI01">WCA</a>
+    </p>
+
+    <h5>主な公式記録（平均）</h5>
+    <p>
+      3x3x3 13.02<br>
+      3x3x3 Fewest Moves 32.00<br>
+      4x4x4 43.15<br>
+      5x5x5 1:32.83<br>
+      Megaminx 1:14.18<br>
+      Pyraminx 6.26<br>
+      Clock 8.98<br>
+      Skewb 7.98
+    </p>
+
+    <h5>主な成績</h5>
+    <p>
+      アジア大会2012 ルービッククロック3位<br>
+      日本大会2013　ルービッククロック3位、ピラミンクス3位<br>
+      アジア大会2014　最少手数3位<br>
+      日本大会2016　最少手数3位
+    </p>
   </div>
 </template>
 
