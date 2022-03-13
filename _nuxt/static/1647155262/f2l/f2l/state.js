0a4b757c2aea65281b848f3d2e77cfd959123cb2
roudai <roudai@users.noreply.github.com>
2022-03-13 07:19:15 +0000

    deploy: 3d6facd75aad5771098a274c2bc2af4141893a74

--- _nuxt/static/1647155262/f2l/f2l/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Froudai.net\u002F_nuxt\u002Fstatic\u002F1647155262",layout:"default",error:a,serverRendered:true,routePath:"\u002Ff2l\u002Ff2l",config:{_app:{basePath:"\u002Froudai.net\u002F",assetsPath:"\u002Froudai.net\u002F_nuxt\u002F",cdnURL:a}}}}(null));
