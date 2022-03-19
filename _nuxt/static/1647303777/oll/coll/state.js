2eda439cf0840d7ae0f868be22bb88b3a96532b5
roudai <roudai@users.noreply.github.com>
2022-03-19 06:15:01 +0000

    deploy: 7df1a38779876e328331e6fbf884e7fa41afb91a

--- _nuxt/static/1647303777/oll/coll/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Froudai.net\u002F_nuxt\u002Fstatic\u002F1647303777",layout:"default",error:a,serverRendered:true,routePath:"\u002Foll\u002Fcoll",config:{_app:{basePath:"\u002Froudai.net\u002F",assetsPath:"\u002Froudai.net\u002F_nuxt\u002F",cdnURL:a}}}}(null));
