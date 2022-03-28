d5cef3233c9d4793f9b687d0952df95fe277bec6
roudai <me@roudai.net>
2022-03-28 10:52:15 +0900

    MegaminxImageEditorリンク追加

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -51,6 +51,7 @@
           
           <b-nav-item-dropdown text="Tools" left>
             <b-dropdown-item href="https://visualcube.roudai.net">VisualCube Editor</b-dropdown-item>
+            <b-dropdown-item href="https://roudai.github.io/MegaminxImageEditor/">Megaminx Image Editor</b-dropdown-item>
           </b-nav-item-dropdown>
         </b-navbar-nav>
 
